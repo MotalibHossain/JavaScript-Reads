43844b9978f70a39e71af848532aefd80415549d
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-01-20 00:02:19 +0600

    update crude operations delate and save

--- DOM/js_crud.js
+++ DOM/js_crud.js
@@ -1,24 +1,25 @@
 $('#SubmitForm').submit(function(event){
     // alert("Enter submit")
     event.preventDefault();
-    SaveFormData()
+    GetFormData()
     $(this).trigger("reset");
     $("#staticBackdrop").modal('hide');
 })
 
-
-function SaveFormData(){
-    let All_data=GetFormData()
-    console.log(All_data)
-}
-
+let sl_no=1
 function GetFormData(){
-    let All_data={}
-    All_data['To_do']=$('#To_do').val();
-    All_data['status']=$('#status').val();
-    return All_data;
+    sl_no=sl_no
+    To_do=$('#To_do').val();
+    To_status=$('#status').val();
+    let Create_row='<tr><td>'+sl_no+'</td>  <td>'+To_do+'</td> <td>'+To_status+'</td> <td><button class="btn btn-success">Edit</button><button  class="btn_delate btn btn-danger ms-3">Delate</button></td></tr>'
+    $('tbody').append(Create_row);
+    sl_no++
 }
 function InsertFormData(){
-    
+
 }
+$(document).on('click', '.btn_delate', function(){
+    $(this).closest('tr').remove();
+    sl_no--
+})
 
